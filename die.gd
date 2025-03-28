52dab846ec7cd5d929f5ce66dec864238765dd9b
radiantmythx <zachary.brewer.git@gmail.com>
2025-03-28 12:02:54 -0500

    Add better UI, add multiple dice, remove ability to left click drag and allow user to hit roll button

--- die.gd
+++ die.gd
@@ -1,26 +1,24 @@
 extends RigidBody3D
 
-@export var face_markers: Array[Marker3D]  # Assign one per face in the editor
-@export var face_values: Array[int]        # Match indices to marker directions
+signal die_landed(die: RigidBody3D, result: int)
+
+@export var face_markers: Array[Marker3D]
+@export var face_values: Array[int]
+
+func _ready():
+	connect("sleeping_state_changed", _on_sleeping_state_changed)
+
+func _on_sleeping_state_changed():
+	if sleeping:
+		var result = get_upward_face()
+		die_landed.emit(self, result)
 
 func get_upward_face() -> int:
 	var highest_y = -INF
 	var top_index = 0
 	for i in face_markers.size():
-		var marker = face_markers[i]
-		var y_pos = marker.global_transform.origin.y
-		if y_pos > highest_y:
-			highest_y = y_pos
+		var y = face_markers[i].global_transform.origin.y
+		if y > highest_y:
+			highest_y = y
 			top_index = i
 	return face_values[top_index]
-
-func _ready():
-	self.connect("sleeping_state_changed", Callable(self, "_on_sleeping_state_changed"))
-
-func _on_sleeping_state_changed():
-	if sleeping:
-		var result = get_upward_face()
-		print("Result:", result)
-		var floating_number = preload("res://FloatingNumber.tscn").instantiate()
-		floating_number.set_number(result)
-		get_tree().current_scene.add_child(floating_number)
--- input_controller.gd
+++ input_controller.gd
@@ -5,12 +5,38 @@ var held_die: RigidBody3D = null
 var grab_offset: Vector3
 var drag_plane_y = 1.0  # The height above ground where the die follows the mouse
 
+var all_dice: Array[RigidBody3D] = []
+var grabbed_dice: Array[RigidBody3D] = []
+var landed_dice: Dictionary = {}  # die: result
+var waiting_for_landing: bool = false
+
+@export var die_scene:PackedScene
+
+func _on_spawn_pressed():
+	var die = die_scene.instantiate()
+	die.global_transform.origin = Vector3(
+		randf_range(-2, 2), 1, randf_range(-2, 2)
+	)
+	die.die_landed.connect(_on_die_landed)
+	get_tree().current_scene.add_child(die)
+	all_dice.append(die)
+
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
 			_try_pickup_die()
 		else:
 			_release_die()
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT:
+		if event.pressed:
+			_pickup_all_dice()
+		if !event.pressed:
+			_release_all_dice()
+
+func _pickup_all_dice():
+	grabbed_dice.clear()
+	for die in all_dice:
+		grabbed_dice.append(die)
 
 func _process(_delta):
 	if held_die:
@@ -60,3 +86,45 @@ func _release_die():
 		held_die.apply_torque_impulse(random_torque)
 		held_die.apply_impulse(Vector3.UP * randf_range(4.0, 6.0))
 		held_die = null
+		
+func _release_all_dice():
+	landed_dice.clear()
+	waiting_for_landing = true
+
+	for die in grabbed_dice:
+		die.freeze = false
+		die.global_transform.origin.y += 3
+		die.apply_torque_impulse(Vector3(
+			randf_range(-50, 50),
+			randf_range(-50, 50),
+			randf_range(-50, 50)
+		))
+	grabbed_dice.clear()
+
+func _on_die_landed(die: RigidBody3D, result: int):
+	if waiting_for_landing and die in all_dice:
+		if not landed_dice.has(die):
+			landed_dice[die] = result
+
+		if landed_dice.size() == all_dice.size():
+			_show_total_result()
+			
+func _show_total_result():
+	waiting_for_landing = false
+	var total = 0
+	for result in landed_dice.values():
+		total += result
+
+	var floating_number = preload("res://FloatingNumber.tscn").instantiate()
+	floating_number.set_number(total)
+	get_tree().current_scene.add_child(floating_number)
+	
+func _on_clear_dice_pressed():
+	for die in all_dice:
+		if is_instance_valid(die):
+			die.queue_free()
+	all_dice.clear()
+	
+func roll_all_dice():
+	_pickup_all_dice()
+	_release_all_dice()
